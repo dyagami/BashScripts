726eb0b6bb381ad4ba8e40438692cdf77e860372
dyagami <hvrryvngel@yahoo.com>
2023-12-24 00:36:37 +0100

    changes

--- tutorial/cruft_remover.sh
+++ tutorial/cruft_remover.sh
@@ -3,7 +3,7 @@
 read -r -p "What folder you want to cruft from?: " folder
 read -r -p "How many days old files to cruft have to be?: " time
 
-readarray -t filelist < <(find -type f -mtime "$time")
+readarray -t filelist < <(find "$folder" -type f -mtime "$time")
 for i in "${filelist[@]}"; do
 	echo "$i"
 	# rm -i "$i"
