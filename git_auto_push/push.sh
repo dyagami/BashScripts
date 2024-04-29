50f9ca0e1f38de07f9865c9fcfc4e6d60f07e2b2
dyagami <dyagamii@proton.me>
2024-04-29 05:54:59 +0000

    auto-generated commit

--- git_auto_push/push.sh
+++ git_auto_push/push.sh
@@ -1,20 +1,49 @@
 #!/bin/bash
 
+BLUE='\033[0;34m'
+GREEN='\033[1;32m'
+NC='\033[0m'
+clear
 for i in $( ls ); do
-	if [[ -d $i  ]] && [[ $i != '.' ]] && [[ $i != '..' ]] ; then
+	if [[ -d $i && "$i" != '.' && "$i" != '..' ]] ; then
+		echo -e "checking: ${BLUE}$i${NC}"
 		cd $i
-		while [[ -z $(git status | grep "Your Branch is ahead" ]] || [[ -z $(git status | grep "Changes not staged") ]]; do
-			if [[ -z $(git status | grep "Your Branch is ahead") ]] ; then
-				echo "$i: pushing commits"
-				git push
+		while [[ -n $(git status | grep "Your branch is ahead") || -n $(git status | grep "Changes to be committed") || -n $(git status | grep "Untracked files") ]]; do
+			if [[ -n $(git status | grep "Your branch is ahead") ]] ; then
+				echo -e "${BLUE}$i${NC}: pushing commits"
+				git push 1>/dev/null
+				echo -e "${BLUE}$i${NC}: pushing commits - ${GREEN}DONE${NC}"
 			fi
-			if [[ -z $(git status | grep "Changes not staged") ]] ; then
-				echo "$i: staging and committing"
-				git add . ; git commit -m "auto-generated commit"
+			if [[ -n $(git status | grep "Changes to be committed") ]]; then
+				echo -e "${BLUE}$i${NC}: committing changes"
+				git commit -m "auto-generated commit" 1>/dev/null
+				echo -e "${BLUE}$i${NC}: committing changes - ${GREEN}DONE${NC}"
+			fi
+			if [[ -n $(git status | grep "Changes not staged") || -n $(git status | grep "Untracked files") ]] ; then
+				echo -e "${BLUE}$i${NC}: staging changes for commit"
+				git add . 1>/dev/null
+				echo -e "${BLUE}$i${NC}: staging changes for commit - ${GREEN}DONE${NC}"
 			fi
 		done
+		echo -e "${BLUE}$i${NC}: checking ${GREEN}done${NC}\n"
 		cd $OLDPWD
 	fi
 done
-#Your branch is ahead
-#Changes not staged
+echo -e "all done!\n"
+#Your branch is up to date with 'origin/master'.
+#nothing to commit, working tree clean
+
+#Your branch is up to date with 'origin/master'.
+#Untracked files:
+#(use "git add <file>..." to include in what will be committed)
+#test
+#nothing added to commit but untracked files present (use "git add" to track)
+
+#Your branch is up to date with 'origin/master'.
+#Changes to be committed:
+#(use "git restore --staged <file>..." to unstage)
+#new file:   test
+
+#Your branch is ahead of 'origin/master' by 1 commit.
+#(use "git push" to publish your local commits)
+#nothing to commit, working tree clean
